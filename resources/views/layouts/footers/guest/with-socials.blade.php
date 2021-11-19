32a0e18b6680995fa96eaf738baccaa189ecdfd2
yogi-boomer <lufevacecbtis13@gmail.com>
2021-11-18 23:08:04 -0600

    first commit

---
+++ resources/views/layouts/footers/guest/with-socials.blade.php
@@ -0,0 +1,15 @@
+  <footer class="footer pb-5">
+      <div class="container">
+          <div class="row">
+              
+          </div>
+          <div class="row">
+              <div class="col-8 mx-auto text-center mt-1">
+                  <p class="mb-0 text-secondary">
+                      Copyright © {{ now()->year }} Creado por <a style="color: #252f40; font-weight:bold"> YIPIStudios</a> 
+              </div>
+              </p>
+          </div>
+      </div>
+      </div>
+  </footer>
---
+++ resources/views/livewire/laravel-examples/user-profile.blade.php
@@ -0,0 +1,101 @@
+<div>
+    <div class="container-fluid">
+        <div class="page-header min-height-150 border-radius-xl mt-4"
+            style="background-image: url('../assets/img/curved-images/curved0.jpg'); background-position-y: 50%;">
+            <span class="mask bg-gradient-primary opacity-6"></span>
+        </div>
+        <div class="card card-body blur shadow-blur mx-4 mt-n6">
+            <a style="font-weight= bold">Nuevo estudiante 1</a>
+        </div>
+    </div>
+
+    <div class="container-fluid py-4">
+        <div class="card">
+            <div class="card-header pb-0 px-3">
+                <h6 class="mb-0">{{ __('Información del estudiante') }}</h6>
+            </div>
+            <div class="card-body pt-4 p-3">
+
+                @if ($showDemoNotification)
+                    <div wire:model="showDemoNotification" class="mt-3  alert alert-primary alert-dismissible fade show"
+                        role="alert">
+                        <span class="alert-text text-white">
+                            {{ __('You are in a demo version, you can\'t update the profile.') }}</span>
+                        <button wire:click="$set('showDemoNotification', false)" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                        </button>
+                    </div>
+                @endif
+
+                @if ($showSuccesNotification)
+                    <div wire:model="showSuccesNotification"
+                        class="mt-3 alert alert-primary alert-dismissible fade show" role="alert">
+                        <span class="alert-icon text-white"><i class="ni ni-like-2"></i></span>
+                        <span
+                            class="alert-text text-white">{{ __('Datos guardados correctamente!') }}</span>
+                        <button wire:click="$set('showSuccesNotification', false)" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                        </button>
+                    </div>
+                @endif
+
+                <form wire:submit.prevent="save" action="#" method="POST" role="form text-left">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="user-name" class="form-control-label">{{ __('Full Name') }}</label>
+                                <div class="@error('user.name')border border-danger rounded-3 @enderror">
+                                    <input wire:model="user.name" class="form-control" type="text" placeholder="Name"
+                                        id="user-name">
+                                </div>
+                                @error('user.name') <div class="text-danger">{{ $message }}</div> @enderror
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="user-email" class="form-control-label">{{ __('Email') }}</label>
+                                <div class="@error('user.email')border border-danger rounded-3 @enderror">
+                                    <input wire:model="user.email" class="form-control" type="email"
+                                        placeholder="@example.com" id="user-email">
+                                </div>
+                                @error('user.email') <div class="text-danger">{{ $message }}</div> @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="user.phone" class="form-control-label">{{ __('Phone') }}</label>
+                                <div class="@error('user.phone')border border-danger rounded-3 @enderror">
+                                    <input wire:model="user.phone" class="form-control" type="tel"
+                                        placeholder="40770888444" id="phone">
+                                </div>
+                                @error('user.phone') <div class="text-danger">{{ $message }}</div> @enderror
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="user.location" class="form-control-label">{{ __('Location') }}</label>
+                                <div class="@error('user.location') border border-danger rounded-3 @enderror">
+                                    <input wire:model="user.location" class="form-control" type="text"
+                                        placeholder="Location" id="name">
+                                </div>
+                                @error('user.location') <div class="text-danger">{{ $message }}</div> @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="about">{{ 'About Me' }}</label>
+                        <div class="@error('user.about')border border-danger rounded-3 @enderror">
+                            <textarea wire:model="user.about" class="form-control" id="about" rows="1"
+                                placeholder="Say something about yourself"></textarea>
+                        </div>
+                        @error('user.about') <div class="text-danger">{{ $message }}</div> @enderror
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <button type="submit" class="btn bg-gradient-dark btn-md mt-4 mb-4">{{ 'Siguiente' }}</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
