220394f0fa9417e41effe3581d695ecd28313326
MaanicW <wariotanks28@gmail.com>
2021-11-21 15:15:50 -0600

    Navbar

--- resources/views/layouts/navbars/auth/nav-profile.blade.php
+++ resources/views/layouts/navbars/auth/nav-profile.blade.php
@@ -3,7 +3,7 @@
         <div class="container-fluid py-1">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 ps-2 me-sm-6 me-5">
-                    <li class="breadcrumb-item text-sm"><a class="text-white opacity-5" href="javascript:;">Pages</a>
+                    <li class="breadcrumb-item text-sm"><a class="text-white opacity-5" href="javascript:;">Página</a>
                     </li>
                     <li class="breadcrumb-item text-sm text-white active text-capitalize" aria-current="page"> {{ str_replace('-', ' ', Route::currentRouteName()) }}</li>
                 </ol>
--- resources/views/livewire/static-sign-up.blade.php
+++ resources/views/livewire/static-sign-up.blade.php
@@ -6,8 +6,6 @@
             <div class="row justify-content-center">
                 <div class="col-lg-5 text-center mx-auto">
                     <h1 class="text-white mb-2 mt-5">Bienvenido!</h1>
-                    <p class="text-lead text-white">Use these awesome forms to login or create new account in your
-                        project for free.</p>
                 </div>
             </div>
         </div>
