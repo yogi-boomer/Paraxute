f4343ff981a23ed8acc1d5095a66d665147648df
MaanicW <wariotanks28@gmail.com>
2021-12-01 11:43:19 -0600

    PDF

--- resources/views/recibos/pdf.blade.php
+++ resources/views/recibos/pdf.blade.php
@@ -5,11 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Generar PDF</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/pdfapp.css') }}" rel="stylesheet">
     <img src="{{ public_path('assets/img/recibo.png') }}" style="width: 700px; height: 900px">
     
 </head>
 <body>
-
+    <div class="nombre">
+        <h1>Hola</h1>
+    </div>
 </body>
 </html>
